1f7efcc9c523d44358c0fd51929c0540e5419c69
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2024-11-02 22:11:49 +0300

    modify project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -22,8 +22,8 @@ const useStyles = makeStyles((theme) => ({
   },
   project: {
     position: "relative",
-    width: "40%",
-    height: "350px",
+    width: "100%",
+    maxWidth: "300px", 
     margin: theme.spacing(2),
     borderRadius: "10px",
     boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
@@ -32,6 +32,18 @@ const useStyles = makeStyles((theme) => ({
     overflow: "hidden",
     "&:hover $description": {
       transform: "translateY(0)",
+    },
+    "&:hover $projectName": {
+      opacity: 0, 
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "45%", 
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "30%", 
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "25%", 
     },
   },
   image: {
@@ -70,64 +82,70 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "center",
     fontSize: "1.1rem",
+    
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  projectName: {
+    transition: "opacity 0.3s ease", 
   },
 }));
-
 
 const Projects = () => {
   const classes = useStyles();
 
-const projects = [
-  {
-    name: "Movie Mania",
-    image: moviemania,
-    link: "https://moviemonia.netlify.app",
-    description:
-      "An interactive application for browsing the top trending movies with various filtering options.",
-  },
-  {
-    name: "Apple Website",
-    image: applewebsite,
-    link: "https://resonant-semolina-5bb316.netlify.app",
-    description:
-      "A responsive replica of Apple's official website, featuring smooth animations and a polished interface.",
-  },
-  {
-    name: "T-movies",
-    image: tmovie,
-    link: "https://github.com/abate-Agegnehu/T-movies",
-    description:
-      "A movie database application that showcases a wide range of popular movies channels and program, with detailed information for each.",
-  },
-  {
-    name: "Dashboard",
-    image: dashboard,
-    link: "https://github.com/abate-Agegnehu/Dashboard-By-React",
-    description:
-      "A highly customizable React-based dashboard with features tailored for data visualization and management.",
-  },
-  {
-    name: "Ecommerce",
-    image: ecommerce,
-    link: "https://github.com/abate-Agegnehu/E-commerce-by-react",
-    description:
-      "A versatile e-commerce platform offering a cart functionality and user-friendly product browsing.",
-  },
-  {
-    name: "Pokmen",
-    image: pokemen,
-    link: "https://github.com/abate-Agegnehu/Pokemon",
-    description:
-      "A fun Pokémon-themed application displaying character information and stats for each Pokémon.",
-  },
-  {
-    name: "Todo-List",
-    image: todolist,
-    link: "https://github.com/abate-Agegnehu/TodoList",
-    description:
-      "An efficient to-do list application that helps users manage and prioritize their daily tasks effectively.",
-  },
-];
+  const projects = [
+    {
+      name: "Movie Mania",
+      image: moviemania,
+      link: "https://moviemonia.netlify.app",
+      description:
+        "An interactive application for browsing the top trending movies with various filtering options.",
+    },
+    {
+      name: "Apple Website",
+      image: applewebsite,
+      link: "https://resonant-semolina-5bb316.netlify.app",
+      description:
+        "A responsive replica of Apple's official website, featuring smooth animations and a polished interface.",
+    },
+    {
+      name: "T-movies",
+      image: tmovie,
+      link: "https://github.com/abate-Agegnehu/T-movies",
+      description:
+        "A movie database application that showcases a wide range of popular movies channels and program, with detailed information for each.",
+    },
+    {
+      name: "Dashboard",
+      image: dashboard,
+      link: "https://github.com/abate-Agegnehu/Dashboard-By-React",
+      description:
+        "A highly customizable React-based dashboard with features tailored for data visualization and management.",
+    },
+    {
+      name: "Ecommerce",
+      image: ecommerce,
+      link: "https://github.com/abate-Agegnehu/E-commerce-by-react",
+      description:
+        "A versatile e-commerce platform offering a cart functionality and user-friendly product browsing.",
+    },
+    {
+      name: "Pokmen",
+      image: pokemen,
+      link: "https://github.com/abate-Agegnehu/Pokemon",
+      description:
+        "A fun Pokémon-themed application displaying character information and stats for each Pokémon.",
+    },
+    {
+      name: "Todo-List",
+      image: todolist,
+      link: "https://github.com/abate-Agegnehu/TodoList",
+      description:
+        "An efficient to-do list application that helps users manage and prioritize their daily tasks effectively.",
+    },
+  ];
 
   return (
     <div className={classes.root}>
@@ -145,7 +163,7 @@ const projects = [
                 className={classes.image}
               />
             </a>
-            <p>{project.name}</p>
+            <p className={classes.projectName}>{project.name}</p>
             <div className={classes.description}>{project.description}</div>
           </div>
         ))}
